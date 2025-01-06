d46a26e59f0ecd3c0f6b58c9a8b273dcadc443b7
Daniel <dmehari147@gmail.com>
2025-01-05 23:23:12 -0600

    first commit

---
+++ constants/index.tsx
@@ -0,0 +1,34 @@
+export const sidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgURL: 'icons/Home.svg',
+    },
+    {
+        label: 'Previous',
+        route: '/previous',
+        imgURL: 'icons/Previous.svg',
+    },
+    {
+        label: 'Upcoming',
+        route: '/upcoming',
+        imgURL: 'icons/Upcoming.svg',
+    },
+    {
+        label: 'Recodings',
+        route: '/recordings',
+        imgURL: 'icons/Video.svg',
+    }, {
+        label: 'Personal Room',
+        route: '/personal-room',
+        imgURL: 'icons/add-personal.svg',
+    },
+]
+
+export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.png',
+    '/images/avatar-4.png',
+    '/images/avatar-5.png',
+]
